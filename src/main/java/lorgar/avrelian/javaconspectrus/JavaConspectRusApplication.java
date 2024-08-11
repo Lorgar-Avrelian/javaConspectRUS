f6a899ceed15d78c30e0043378d5010a89027510
Lorgar-Avrelian <victor-14-244@mail.ru>
2024-08-11 08:25:34 +0300

    Project initialization

---
+++ src/main/java/lorgar/avrelian/javaconspectrus/JavaConspectRusApplication.java
@@ -0,0 +1,13 @@
+package lorgar.avrelian.javaconspectrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConspectRusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaConspectRusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lorgar/avrelian/javaconspectrus/JavaConspectRusApplicationTests.java
@@ -0,0 +1,13 @@
+package lorgar.avrelian.javaconspectrus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConspectRusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
