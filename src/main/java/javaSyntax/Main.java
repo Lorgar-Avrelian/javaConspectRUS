0deed21cd06eb4cef448a513ccda139ef7e25dd1
Lorgar-Avrelian <victor-14-244@mail.ru>
2024-08-13 20:07:19 +0300

    In progress

--- src/main/java/javaSyntax/Main.java
+++ src/main/java/javaSyntax/Main.java
@@ -1,10 +1,71 @@
 package javaSyntax;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         switchMethod(2);
         dayOfWeek(4);
         ternarOperator(1, 2);
+        forExample();
+        whileExample();
+        doWhileExample();
+        forEachExample();
+        arraysExample();
+    }
+
+    private static void arraysExample() {
+        int[] array1 = {1, 2, 3};
+        int[][] array2 = {
+                {1, 2, 3},
+                {4, 5, 6, 7},
+                {8, 9, 10}
+        };
+        int[] array3 = new int[]{1, 2, 3};
+        int[][] array4 = new int[][]{
+                array3,
+                {4, 5, 6, 7},
+                {8, 9, 10}
+        };
+        int array5[] = array1;
+        int array6[][] = array2;
+        System.out.println(Arrays.toString(array5));
+        System.out.println(Arrays.toString(array6[0]));
+        System.out.println(Arrays.toString(array6[1]));
+        System.out.println(Arrays.toString(array6[2]));
+        System.out.println(Arrays.equals(array4, array2));
+        System.out.println(Arrays.equals(array3, array1));
+    }
+
+    private static void forEachExample() {
+        List<Integer> testList = new ArrayList<>(List.of(1, 2, 3));
+        for (Integer i : testList) {
+            System.out.println("i = " + i);
+        }
+    }
+
+    private static void doWhileExample() {
+        int i = 0;
+        do {
+            action1();
+            i++;
+        } while (i < 10);
+    }
+
+    private static void whileExample() {
+        int i = 10;
+        while (i > 0) {
+            action1();
+            i--;
+        }
+    }
+
+    private static void forExample() {
+        for (int i = 0; i < 10; i++) {
+            action1();
+        }
     }
 
     private static void ternarOperator(int a, int b) {
