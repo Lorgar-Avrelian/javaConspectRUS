3fa925859bb75a3081c73be11e6cdbc695ab01dd
Lorgar-Avrelian <victor-14-244@mail.ru>
2024-09-11 13:47:22 +0300

    In progress

--- src/main/java/javaCore/Main.java
+++ src/main/java/javaCore/Main.java
@@ -3,9 +3,30 @@ package javaCore;
 import javaCore.models.Person;
 import javaCore.models.Worker;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
 public class Main {
     public static void main(String[] args) {
-        inheritanceExample();
+//        inheritanceExample();
+        tryCatchFinallyExample();
+    }
+
+    private static void tryCatchFinallyExample() {
+        try {
+            File file = new File("pictures/16.png");
+            System.out.println(Files.size(file.toPath()));
+        } catch (NoSuchElementException e) {
+            System.out.println("File not found!");
+        } catch (IOException | ClassCastException e) {
+            System.out.println("Internal error!");
+        } finally {
+            System.out.println("The end");
+        }
     }
 
     private static void inheritanceExample() {
