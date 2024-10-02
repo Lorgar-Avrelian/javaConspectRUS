f1fb40c3fa38fe8d77318e33f79193d86f6f5a56
Lorgar-Avrelian <victor-14-244@mail.ru>
2024-10-02 08:44:57 +0300

    In progress

---
+++ src/main/java/files/Main.java
@@ -0,0 +1,22 @@
+package files;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            filesExample();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void filesExample() throws IOException {
+        Path path = Path.of("books/covers/1.jpg");
+        System.out.println(path);
+        boolean b = Files.exists(path);
+        System.out.println(Files.probeContentType(path));
+    }
+}
