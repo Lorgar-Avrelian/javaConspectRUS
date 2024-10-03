0fd5c14aeb7e26bce277168fde3db8aaca3bd894
Lorgar-Avrelian <victor-14-244@mail.ru>
2024-10-03 16:36:29 +0300

    In progress

---
+++ src/test/java/lorgar/avrelian/javaconspectrus/controllers/BooksControllerTest.java
@@ -0,0 +1,40 @@
+package lorgar.avrelian.javaconspectrus.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BooksControllerTest {
+
+    @Test
+    void createBook() {
+    }
+
+    @Test
+    void readBook() {
+    }
+
+    @Test
+    void updateBook() {
+    }
+
+    @Test
+    void deleteBook() {
+    }
+
+    @Test
+    void getAllBooks() {
+    }
+
+    @Test
+    void uploadCover() {
+    }
+
+    @Test
+    void downloadCover() {
+    }
+
+    @Test
+    void testDownloadCover() {
+    }
+}
