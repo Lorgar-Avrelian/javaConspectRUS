7d0156d794eecb7c35740a823d670ba359c39268
Lorgar-Avrelian <victor-14-244@mail.ru>
2024-11-01 13:54:17 +0300

    In progress

---
+++ src/main/java/lorgar/avrelian/javaconspectrus/configurations/CachingConfiguration.java
@@ -0,0 +1,21 @@
+package lorgar.avrelian.javaconspectrus.configurations;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@EnableCaching
+public class CachingConfiguration {
+    @Bean
+    public CacheManager cacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(List.of(new ConcurrentMapCache("book")));
+        return cacheManager;
+    }
+}
