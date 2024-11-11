c22660e0ff179b36ea8120659ada27581fd95649
Lorgar-Avrelian <victor-14-244@mail.ru>
2024-11-11 14:49:36 +0300

    in progress

--- src/main/java/lorgar/avrelian/javaconspectrus/models/Role.java
+++ src/main/java/lorgar/avrelian/javaconspectrus/models/Role.java
@@ -1,5 +1,12 @@
 package lorgar.avrelian.javaconspectrus.models;
 
-public enum Role {
-    USER, ADMIN, GUEST
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER, ADMIN, OWNER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
---
+++ src/main/java/lorgar/avrelian/javaconspectrus/services/JdbcUserDetailsService.java
@@ -0,0 +1,8 @@
+package lorgar.avrelian.javaconspectrus.services;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+//@Service
+public interface JdbcUserDetailsService extends UserDetailsService {
+}
