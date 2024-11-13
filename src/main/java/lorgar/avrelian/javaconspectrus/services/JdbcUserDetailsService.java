d7a27ac7a4b43b53d99ff22d233e7eae84196272
Lorgar-Avrelian <victor-14-244@mail.ru>
2024-11-13 11:04:47 +0300

    in progress

--- src/main/java/lorgar/avrelian/javaconspectrus/services/JdbcUserDetailsService.java
+++
@@ -1,8 +0,0 @@
-package lorgar.avrelian.javaconspectrus.services;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.stereotype.Service;
-
-//@Service
-public interface JdbcUserDetailsService extends UserDetailsService {
-}
