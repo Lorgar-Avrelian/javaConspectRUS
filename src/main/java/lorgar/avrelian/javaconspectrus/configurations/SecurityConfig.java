68c9cbdc2838331a6a607e56e4fbc9def52881d0
Lorgar-Avrelian <victor-14-244@mail.ru>
2024-11-14 19:49:13 +0300

    In progress

--- src/main/java/lorgar/avrelian/javaconspectrus/configurations/SecurityConfig.java
+++ src/main/java/lorgar/avrelian/javaconspectrus/configurations/SecurityConfig.java
@@ -1,12 +1,19 @@
 package lorgar.avrelian.javaconspectrus.configurations;
 
 import lorgar.avrelian.javaconspectrus.securityFilters.BasicAuthCorsFilter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfAuthenticationStrategy;
@@ -15,10 +22,20 @@ import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.time.Duration;
+import java.util.List;
 
 // Включает поддержку Spring Security
 @EnableWebSecurity
 public class SecurityConfig {
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(@Qualifier(value = "jpaUserDetailsServiceImpl") UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -52,6 +69,36 @@ public class SecurityConfig {
                         // осуществляться защита от CSRF-атак
                         .requireCsrfProtectionMatcher(new MediaTypeRequestMatcher(MediaType.APPLICATION_JSON))
                      )
+                .cors(configurer -> {
+                    // Источник конфигураций CORS
+                    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                    // Конфигурация CORS
+                    CorsConfiguration corsConfiguration = new CorsConfiguration();
+                    // Разрешаются CORS-запросы:
+                    // - с сайта http://localhost:8080
+                    corsConfiguration.addAllowedOrigin("http://localhost:8080");
+                    // - с нестандартными заголовками Authorization и X-CUSTOM-HEADER
+                    corsConfiguration.addAllowedHeader(HttpHeaders.AUTHORIZATION);
+                    corsConfiguration.addAllowedHeader("X-CUSTOM-HEADER");
+                    // - с передачей учётных данных
+                    corsConfiguration.setAllowCredentials(true);
+                    // - с методами GET, POST, PUT, PATCH и DELETE
+                    corsConfiguration.setAllowedMethods(List.of(
+                            HttpMethod.GET.name(),
+                            HttpMethod.POST.name(),
+                            HttpMethod.PUT.name(),
+                            HttpMethod.PATCH.name(),
+                            HttpMethod.DELETE.name()
+                                                               ));
+                    // JavaScript может обращаться к заголовку X-OTHER-CUSTOM-HEADER ответа
+                    corsConfiguration.setExposedHeaders(List.of("X-OTHER-CUSTOM-HEADER"));
+                    // Браузер может кешировать настройки CORS на 10 секунд
+                    corsConfiguration.setMaxAge(Duration.ofSeconds(10));
+                    // Использование конфигурации CORS для всех запросов
+                    source.registerCorsConfiguration("/**", corsConfiguration);
+                    // Возврат настроенного фильтра
+                    configurer.configurationSource(source);
+                })
                 // разрешить доступ только для аутентифицированных пользователей
                 .authorizeHttpRequests(
                         requests -> requests
@@ -86,5 +133,18 @@ public class SecurityConfig {
                 .addFilterBefore(new BasicAuthCorsFilter(), UsernamePasswordAuthenticationFilter.class)
                 // собрать цепочку фильтров
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
